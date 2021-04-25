8b758e15a2ac8ca18999e40e24d3428794b08438
Don <danishakh@gmail.com>
2021-04-24 17:18:14 -0700

    initial commit - with firebase config

---
+++ src/firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+	apiKey: "AIzaSyBBlBNl1nwSF5hqZIX31FfXTMSZIU5tEmc",
+	authDomain: "clone-b2804.firebaseapp.com",
+	projectId: "clone-b2804",
+	storageBucket: "clone-b2804.appspot.com",
+	messagingSenderId: "656881796978",
+	appId: "1:656881796978:web:31014532c309190f47bfeb",
+	measurementId: "G-KCDSM0SW33",
+};
