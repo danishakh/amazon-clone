998c0800bae83a6799971a1ef0ee8b4671e6899c
Don <danishakh@gmail.com>
2021-05-04 22:55:56 -0700

    added login component/page

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import "./App.css";
 import Header from "./components/Header/Header";
 import Home from "./components/Home/Home";
 import Checkout from "./components/Checkout/Checkout";
+import Login from "./components/Login/Login";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 function App() {
@@ -9,12 +10,16 @@ function App() {
 		// BEM
 		<Router>
 			<div className="app">
-				<Header />
 				<Switch>
+					<Route path="/login">
+						<Login />
+					</Route>
 					<Route path="/checkout">
+						<Header />
 						<Checkout />
 					</Route>
 					<Route path="/">
+						<Header />
 						<Home />
 					</Route>
 				</Switch>
